4a5c7f2a95f465b7b6b04a4b4ff11fa14b79eee9
Jeseekia Vaughn <jeseekia@gmail.com>
2023-06-07 02:49:53 -0400

    Implement chat data storage to mongodb collection from Twitch chat

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+require('dotenv').config()
 const tmi = require('tmi.js');
+const { MongoClient } = require("mongodb");
 
-const client = new tmi.Client({
-	channels: [ 'metadevgirl' ]
-});
+const uri = process.env.DB_CONNECTION_STRING;
 
-client.connect();
+async function run() {
+  try {
+    const mongoClient = new MongoClient(uri);
+    await mongoClient.connect();
+    const db = mongoClient.db("TwitchChat")
+    const msgColl = db.collection("Messages")
+    const dataColl = db.collection("MessageClassification")
 
-client.on('message', (channel, tags, message, self) => {
-	console.log(`${tags['display-name']}: ${message}`);
-});
+    const tmiClient = new tmi.Client({
+        channels: [ 'metadevgirl' ]
+    });
+
+    await tmiClient.connect();
+    tmiClient.on('message', (channel, tags, message, self) => {
+        console.log(`${tags['display-name']}: ${message}`);
+        msgColl.insertOne({
+            "message": message,
+            "user": tags.username,
+            "channel": channel,
+            "timestamp": new Date(tags['tmi-sent-ts'])
+        });
+        dataColl.insertOne({
+            "prompt": message,
+            "completion": ""
+        });
+    });
+  } catch (err) {
+        console.error("Error:", err);
+        process.exit(1);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoClient.close();
+  }
+}
+run().catch(console.dir);
